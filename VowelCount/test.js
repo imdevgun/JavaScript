4f0677010ac28f7055e60a6f3f7e67f8fd7acb8c
Sahil Sharma <devgun.sahil@icloud.com>
2020-07-22 21:35:20 -0400

    final commit for vowels

--- VowelCount/test.js
+++ VowelCount/test.js
@@ -2,8 +2,11 @@
 // Write a program that returns the total numbers of vowels in a word
 
 function vowelCount(word){
+    // a count variable to count the number of vowels
     count = 0
+    // convert all letters to lowercase for case-sensitive inputs
     word = word.toLowerCase()
+    // loop through the word checking for vowels at every character. REMEMBER a string is nothing but an ARRAY of characters
     for(letter of word){
         if(letter === 'a' || letter === 'e' || letter === 'i' || letter === 'o' || letter === 'u') {
             count++
